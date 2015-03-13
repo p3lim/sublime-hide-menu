7c5caa753f3f9e5398c485f035fdcac33003da70
Adrian L Lange <mail@p3lim.net>
2015-03-13 23:32:32 +0100

    Be gone!

---
+++ sublime-hide-menu.py
@@ -0,0 +1,10 @@
+from sublime_plugin import EventListener
+
+known_windows = []
+
+class HideMenu(EventListener):
+	def on_new(self, view):
+		window = view.window()
+		if not window.id() in known_windows:
+			known_windows.append(window.id())
+			window.run_command("toggle_menu")
