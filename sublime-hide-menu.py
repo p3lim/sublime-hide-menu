a06162ba8ccc666d3e205771916a32c3fd68b479
Adrian L Lange <mail@p3lim.net>
2017-10-25 16:20:14 +0200

    Use provided API to check/hide the menu

--- sublime-hide-menu.py
+++ sublime-hide-menu.py
@@ -1,15 +1,8 @@
 import sublime
 from sublime_plugin import EventListener
 
-known_windows = []
-
 class HideMenu(EventListener):
 	def on_new(self, view):
 		window = view.window()
-		if not window.id() in known_windows:
-			known_windows.append(window.id())
-			window.run_command("toggle_menu")
-
-def plugin_loaded():
-	for window in sublime.windows():
-		known_windows.append(window.id())
+		if window.is_menu_visible():
+			window.set_menu_visible(False)
