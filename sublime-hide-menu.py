44a24adab5890be2ac5f69764ae6d334135e70bd
Adrian L Lange <mail@p3lim.net>
2017-10-25 16:20:47 +0200

    Don't load on OSX (they don't have menus in the windows)
    Fixes #1

--- sublime-hide-menu.py
+++ sublime-hide-menu.py
@@ -1,8 +1,9 @@
 import sublime
 from sublime_plugin import EventListener
 
-class HideMenu(EventListener):
-	def on_new(self, view):
-		window = view.window()
-		if window.is_menu_visible():
-			window.set_menu_visible(False)
+if sublime.platform() not in 'osx':
+	class HideMenu(EventListener):
+		def on_new(self, view):
+			window = view.window()
+			if window.is_menu_visible():
+				window.set_menu_visible(False)
